921e08f1e8a8c2f10e96ebb0d93c26b1e7e10eed
B I Mohammed Abbas <the10minoverview@gmail.com>
2022-08-30 17:12:13 +0530

    Update settings.py

--- museum/museum/settings.py
+++ museum/museum/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'museum',
         'USER': 'root',
-        'PASSWORD': 'yes',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '',
     }
